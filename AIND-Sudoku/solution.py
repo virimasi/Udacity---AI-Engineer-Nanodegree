68db7be1e9d85ca129a877800babd5ed2454e428
Vinicius Ribeiro Machado da Silva <vini@poli.ufrj.br>
2018-02-04 11:03:13 -0200

    Add files via upload

---
+++ AIND-Sudoku/solution.py
@@ -0,0 +1,146 @@
+
+from utils import *
+from collections import Counter
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+
+# TODO: Update the unit list to add the new diagonal units
+diagonal_units = [[rows[i]+cols[i] for i in range(len(rows))] , [rows[-i-1]+cols[i] for i in range(len(rows))]]
+unitlist = unitlist + diagonal_units
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+
+def naked_twins(values):
+    # TODO: Implement this function!
+    # List of boxes with two digits
+    two_digit_boxes = [boxes for boxes in values.keys() if len(values[boxes]) == 2]
+    # Find the twins among the list evaluated before
+    twins = []
+    for box1 in two_digit_boxes:
+        for box2 in peers[box1]:
+            if set(values[box1]) == set(values[box2]):
+                twins.append([box1, box2])
+    
+    # For each pair of naked twins,
+    for i in range(len(twins)):
+        # Box of each pair
+        box_pair1 = twins[i][0]
+        box_pair2 = twins[i][1]
+        # Peers of each box_pair
+        peers_pair1 = set(peers[box_pair1])
+        peers_pair2 = set(peers[box_pair2])
+        # Peers in common between twins
+        peers_in_common = peers_pair1 & peers_pair2
+
+        # Removing the
+        for box in peers_in_common:
+            if len(values[box]) > 1:
+                for digit in values[box_pair1]:
+                    values[box] = values[box].replace(digit, '')
+
+    return values
+
+
+def eliminate(values):
+    # TODO: Copy your code from the classroom to complete this function
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+
+def only_choice(values):
+    # TODO: Copy your code from the classroom to complete this function
+    
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+
+def reduce_puzzle(values):
+    # TODO: Copy your code from the classroom and modify it to complete this function
+    
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        # Applying Naked Twins Strategy
+        values = naked_twins(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+
+def search(values):
+    # TODO: Copy your code from the classroom to complete this function
+    
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+def solve(grid):
+    """Find the solution to a Sudoku puzzle using search and constraint propagation
+
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+
+    Returns
+    -------
+    dict or False
+        The dictionary representation of the final sudoku grid or False if no solution exists.
+    """
+    values = grid2values(grid)
+    values = search(values)
+    return values
+
+
+if __name__ == "__main__":
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(grid2values(diag_sudoku_grid))
+    result = solve(diag_sudoku_grid)
+    display(result)
+
+    try:
+        import PySudoku
+        PySudoku.play(grid2values(diag_sudoku_grid), result, history)
+
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
